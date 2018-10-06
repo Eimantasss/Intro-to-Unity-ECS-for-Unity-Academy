210a73ba62b3013f6e6dc98c1aeb847f78fb614e
Eimantasss <43904898+Eimantasss@users.noreply.github.com>
2018-10-06 23:46:21 +0300

    Tinkering with scripts

--- ECS boiii/Assets/Classic/Movement.cs
+++ ECS boiii/Assets/Classic/Movement.cs
@@ -7,17 +7,25 @@ public class Movement : MonoBehaviour {
     public float speed;
     Vector3 position;
 
+    static GameObject TopBound;
+    float TopBoundPos = TopBound.transform.position.x;
+
     // Use this for initialization
     void Start ()
     {
         //Vector3 position = transform.position;
+        //transform.Rotate(Vector3.forward, Random.Range(0.0f, 359.0f));
+        
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         Vector3 position = transform.position;
-        position += -transform.up * speed * Time.deltaTime;
+        position += transform.up * speed * Time.deltaTime;
         transform.position = position;
+
+        if (position.x < TopBoundPos)
+            speed = speed * -1;
 	}
 }
