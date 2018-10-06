868a0b35328aaaf1bb925ea3c058b2aab9c72848
Eimantasss <43904898+Eimantasss@users.noreply.github.com>
2018-10-06 17:59:19 +0300

    Basics
    Basic movement, environment, performance stats, etc.

---
+++ ECS boiii/Assets/Classic/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+    Vector3 position;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Vector3 position = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 position = transform.position;
+        position += -transform.up * speed * Time.deltaTime;
+        transform.position = position;
+	}
+}
---
+++ ECS boiii/Assets/PerformanceStats.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerformanceStats : MonoBehaviour
+{
+    public UnityEngine.UI.Text LabelFrameTime;
+    public UnityEngine.UI.Text LabelFramesPerSecond;
+
+    int FrameRate;
+    float CpuTime;
+
+    int _Frames;
+    float _Time;
+
+    void Start()
+    {
+        Reset();
+    }
+
+    void Reset()
+    {
+        _Time = Time.unscaledTime;
+        _Frames = Time.frameCount;
+    }
+
+    void Update()
+    {
+        var difference = Time.unscaledTime - _Time;
+        if (difference >= 1.0f)
+        {
+            FrameRate = Time.frameCount - _Frames;
+            CpuTime = (difference / FrameRate) * 1000;
+
+            LabelFramesPerSecond.text = string.Format("{0} fps", FrameRate);
+            LabelFrameTime.text = string.Format("{0:F2} ms", CpuTime);
+
+            Reset();
+        }
+    }
+}
