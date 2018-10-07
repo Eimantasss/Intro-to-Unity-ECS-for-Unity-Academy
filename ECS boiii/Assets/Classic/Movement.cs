2ce0a4a796ba2068ffdd706cbb47d4cc69165b61
Eimantasss <43904898+Eimantasss@users.noreply.github.com>
2018-10-07 13:25:53 +0300

    Edge collisions
    Gameobjects now bounce off of the edges of the screen

--- ECS boiii/Assets/Classic/Movement.cs
+++ ECS boiii/Assets/Classic/Movement.cs
@@ -5,27 +5,47 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
     public float speed;
+    public float hspeed;
     Vector3 position;
 
-    static GameObject TopBound;
-    float TopBoundPos = TopBound.transform.position.x;
+    //For checking when to bounce off of the edges
+    public GameObject TopBound;
+    public GameObject BotBound;
+    public GameObject LeftBound;
+    public GameObject RightBound;
+    float TopBoundPos;
+    float BotBoundPos;
+    float LeftBoundPos;
+    float RightBoundPos;
 
     // Use this for initialization
     void Start ()
     {
         //Vector3 position = transform.position;
         //transform.Rotate(Vector3.forward, Random.Range(0.0f, 359.0f));
-        
+        TopBoundPos = TopBound.transform.position.y;
+        BotBoundPos = BotBound.transform.position.y;
+        LeftBoundPos = LeftBound.transform.position.x;
+        RightBoundPos = RightBound.transform.position.x;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
         Vector3 position = transform.position;
         position += transform.up * speed * Time.deltaTime;
+        position += transform.right * hspeed * Time.deltaTime;
         transform.position = position;
 
-        if (position.x < TopBoundPos)
+
+        if (position.y > TopBoundPos)
             speed = speed * -1;
-	}
+        else if (position.y < BotBoundPos)
+            speed = speed * -1;
+        else if (position.x < LeftBoundPos)
+            hspeed = hspeed * -1;
+        else if (position.x > RightBoundPos)
+            hspeed = hspeed * -1;
+    }
 }
